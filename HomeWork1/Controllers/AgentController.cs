808e94cfe1af6139136f747ba961760095116231
TheyCallMeZong <75227915+TheyCallMeZong@users.noreply.github.com>
2021-03-25 18:18:59 +0300

    Update AgentController.cs

--- HomeWork1/Controllers/AgentController.cs
+++ HomeWork1/Controllers/AgentController.cs
@@ -11,6 +11,10 @@ namespace HomeWork1.Controllers
     [ApiController]
     public class AgentController : ControllerBase
     {
+    //как я понял, нужно еще создать класс в котором будет храниться список пользователей List<AgentInfo>();, а в данном контроллере создать экземпляр этого класса.
+    //И каждый раз когда мы будем регестрировать или что то делать 
+    //в этом контроллере нужно будет вызывать конструктор, в котором будем заполять лист Агентов
+    // public AgentController(AgentInfo agentInfo){ (наша локальная переменная с листом агентов) = agentInfo; } примерно так
         [HttpPost("register")]
         public IActionResult RegisterAgent([FromBody] AgentInfo agentInfo)
         {
