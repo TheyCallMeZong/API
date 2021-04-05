aac3c4373174575a1b1667117bfc41908320d892
Семён Савченко <savhenko10@mail.ru>
2021-04-05 23:07:18 +0700

    addedmock testing

--- MetricsAgentTests/UnitTest1.cs
+++ MetricsAgentTests/UnitTest1.cs
@@ -4,45 +4,76 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
 using MetricsCommon;
+using Moq;
+using MetricsAgent.DB.IRepository;
+using Microsoft.Extensions.Logging;
+using MetricsAgent.DB.Data;
 
 namespace MetricsManagerTests
 {
     public class MetricsAhentTests
     {
+        #region
         private NetWorkMetricsController netWorkMetricsController;
+        private Mock<IRepositoryNetWorkMetrics> mockNetWork;
+        private Mock<ILogger<NetWorkMetricsController>> mockLoggingNetWork; 
         private DotNetMetricsController dotNetMetricsController;
+        private Mock<IRepositoryDotNetMetrics> mockDotNetMock;
+        private Mock<ILogger<DotNetMetricsController>> mockLoggingDotNet;
         private CpuMetricsController controller;
+        private Mock<IRepositoryCpuMetrics> mockCpuMetrcs;
+        private Mock<ILogger<CpuMetricsController>> mockLoggingCpuMetrics;
         private HddMetricsController hddMetricsController;
+        private Mock<IRepositoryHddMetrics> mockHddMetrics;
+        private Mock<ILogger<HddMetricsController>> mockLoggingHddMetrics;
         private RamMetricsController ramMetricsController;
+        private Mock<IRepositoryRamMetrics> mockRamMetrics;
+        private Mock<ILogger<RamMetricsController>> mockLoggingRamMetrics;
+        #endregion
         public MetricsAhentTests()
         {
-            ramMetricsController = new RamMetricsController();
-            netWorkMetricsController = new NetWorkMetricsController();
-            controller = new CpuMetricsController();
-            dotNetMetricsController = new DotNetMetricsController();
-            hddMetricsController = new HddMetricsController();
+            mockNetWork = new Mock<IRepositoryNetWorkMetrics>();
+            mockLoggingNetWork = new Mock<ILogger<NetWorkMetricsController>>();
+            netWorkMetricsController = new NetWorkMetricsController(mockLoggingNetWork.Object, mockNetWork.Object);
+            mockDotNetMock = new Mock<IRepositoryDotNetMetrics>();
+            mockLoggingDotNet = new Mock<ILogger<DotNetMetricsController>>();
+            dotNetMetricsController = new DotNetMetricsController(mockLoggingDotNet.Object, mockDotNetMock.Object);
+            mockCpuMetrcs = new Mock<IRepositoryCpuMetrics>();
+            mockLoggingCpuMetrics = new Mock<ILogger<CpuMetricsController>>();
+            controller = new CpuMetricsController(mockLoggingCpuMetrics.Object, mockCpuMetrcs.Object);
+            mockHddMetrics = new Mock<IRepositoryHddMetrics>();
+            mockLoggingHddMetrics = new Mock<ILogger<HddMetricsController>>();
+            hddMetricsController = new HddMetricsController(mockLoggingHddMetrics.Object, mockHddMetrics.Object);
+            mockRamMetrics = new Mock<IRepositoryRamMetrics>();
+            mockLoggingRamMetrics = new Mock<ILogger<RamMetricsController>>();
+            ramMetricsController = new RamMetricsController(mockLoggingRamMetrics.Object, mockRamMetrics.Object);
         }
         TimeSpan fromTime = TimeSpan.FromSeconds(0);
         TimeSpan toTime = TimeSpan.FromSeconds(100);
+        Percentile percentile = Percentile.P95;
+
         #region
         [Fact]
         public void GetMetricsCpuByPercentile_ReturnOk()
         {
-            var percentile = Percentile.P95;
-            _ = Assert.IsAssignableFrom<IActionResult>(controller.GetMetricsByPercentile(fromTime, toTime, percentile));
+            mockCpuMetrcs.Setup(r => r.Create(It.IsAny<CpuMetrics>())).Verifiable(); 
+            mockCpuMetrcs.Verify(repository => repository.Create(It.IsAny<CpuMetrics>()), Times.AtMostOnce());
+
         }
 
         [Fact]
         public void GetMetricsCpu_ReturnOk()
         {
-            _ = Assert.IsAssignableFrom<IActionResult>(controller.GetMetrics(fromTime, toTime));
+            mockCpuMetrcs.Setup(r => r.Create(It.IsAny<CpuMetrics>())).Verifiable();
+            mockCpuMetrcs.Verify(repository => repository.Create(It.IsAny<CpuMetrics>()), Times.AtMostOnce());
         }
         #endregion
         #region
         [Fact]
         public void GetErrors_ReturnOk()
         {
-            _ = Assert.IsAssignableFrom<IActionResult>(dotNetMetricsController.GetErrors(fromTime, toTime));
+            mockDotNetMock.Setup(r => r.Create(It.IsAny<DotNetMetrics>())).Verifiable();
+            mockDotNetMock.Verify(repository => repository.Create(It.IsAny<DotNetMetrics>()), Times.AtMostOnce());
         }
 
         #endregion
@@ -50,21 +81,24 @@ namespace MetricsManagerTests
         [Fact]
         public void GetFreeSpaceSize_ReturnOk()
         {
-            _ = Assert.IsAssignableFrom<IActionResult>(hddMetricsController.GetFreeSpaceSize());
+            mockHddMetrics.Setup(r => r.Create(It.IsAny<HddMetrics>())).Verifiable();
+            mockHddMetrics.Verify(r => r.Create(It.IsAny<HddMetrics>()), Times.AtLeastOnce());
         }
         #endregion
         #region
         [Fact]
         public void GetMetricsInNetWork_ReturnOk()
         {
-            _ = Assert.IsAssignableFrom<IActionResult>(netWorkMetricsController.GetMetrics(fromTime, toTime));
+            mockNetWork.Setup(r => r.Create(It.IsAny<NetWorkMetrics>())).Verifiable();
+            mockNetWork.Verify(repository => repository.Create(It.IsAny<NetWorkMetrics>()), Times.AtMostOnce());
         }
         #endregion
         #region
         [Fact]
         public void GetFreeSpaceSizeInRam_ReturnOk()
         {
-            _ = Assert.IsAssignableFrom<IActionResult>(ramMetricsController.GetFreeSpaceSize());
+            mockRamMetrics.Setup(r => r.Create(It.IsAny<RamMetrics>())).Verifiable();
+            mockRamMetrics.Verify(r => r.Create(It.IsAny<RamMetrics>()), Times.AtLeastOnce());
         }
         #endregion
     }
